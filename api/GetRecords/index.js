45ddb6cfbd86f362d623e0300f3bc2abe9856684
Oliver Xie <oliverxie19@gmail.com>
2021-05-08 21:02:06 -0700

    node API Azure Function

---
+++ api/GetRecords/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    context.res = {
+        body: [
+            "first",
+            "second"
+        ]
+    };
+}
